8003bafe4f5edf41b98b273b24d43d11251f8c3c
emeraldjava <paul.t.oconnell@gmail.com>
2022-09-06 11:59:20 -0500

    Add gtag code

--- gatsby-config.js
+++ gatsby-config.js
@@ -81,6 +81,7 @@ module.exports = {
       options: {
         // You can add multiple tracking ids and a pageview event will be fired for all of them.
         trackingIds: [
+          "330707515",
           "UA-156016480-1" // Google Analytics / GA
         ],
         // This object gets passed directly to the gtag config command
