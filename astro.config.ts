27b8c25724154231f708906a66b959743a72476e
emeraldjava <paul.t.oconnell@gmail.com>
2025-02-10 19:29:27 +0000

    Add astro-expressive-code

--- astro.config.ts
+++ astro.config.ts
@@ -6,6 +6,8 @@ import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
 
+import expressiveCode from "astro-expressive-code";
+
 // https://astro.build/config
 export default defineConfig({
   // https://docs.astro.build/en/reference/configuration-reference/#site
@@ -18,13 +20,9 @@ export default defineConfig({
   // https://vitejs.dev/guide/env-and-mode.html#env-files
   //base: import.meta.env.DEV ? "." : "/emeraldjava-astro",
   //base: import.meta.env.VITE_BASE,
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    react(),
-    sitemap(),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false,
+  }), react(), sitemap(), expressiveCode()],
   image: {
     // see https://docs.astro.build/en/reference/errors/missing-sharp/
     service: passthroughImageService(),
